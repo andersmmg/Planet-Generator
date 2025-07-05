bc2599b75391b1432630a0c1e7fc8f654a9ae2ff
Joshua Anderson <andersmmg@gmail.com>
2025-07-05 14:01:59 -0600

    Removed unnecessary custom type
    All types use custom classes anyway

--- addons/hoimar.planetgen/plugin.gd
+++ addons/hoimar.planetgen/plugin.gd
@@ -6,9 +6,7 @@ extends EditorPlugin
 ## which can be used to create and manage celestial bodies.
 ## It also adds an autoload singleton "PGGlobals" for global access to plugin-related data.
 
-const CUSTOM_TYPE_NAME := "Solar System"
 const PLUGIN_ICON := preload("resources/icons/solar_system.svg")
-const SOLAR_SYSTEM_SCRIPT := preload("scripts/celestial_bodies/solar_system.gd")
 const PG_GLOBALS_PATH = "res://addons/hoimar.planetgen/scripts/utils/pg_globals.gd"
 
 
@@ -16,18 +14,11 @@ const PG_GLOBALS_PATH = "res://addons/hoimar.planetgen/scripts/utils/pg_globals.
 ## Adds the "PGGlobals" autoload singleton and the "Solar System" custom type.
 func _enter_tree():
 	add_autoload_singleton("PGGlobals", PG_GLOBALS_PATH)
-	add_custom_type(
-		CUSTOM_TYPE_NAME,
-		"Node3D",
-		SOLAR_SYSTEM_SCRIPT,
-		PLUGIN_ICON
-	)
 
 
 ## Called when the plugin is deactivated.
 ## Removes the "Solar System" custom type and the "PGGlobals" autoload singleton.
 func _exit_tree():
-	remove_custom_type(CUSTOM_TYPE_NAME)
 	remove_autoload_singleton("PGGlobals")
 
 
